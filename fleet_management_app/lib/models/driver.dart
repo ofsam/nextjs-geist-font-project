0ffb638beb114eb5e404712b30ad66fa5be5a940
ofori samuel <62040848+ofsam@users.noreply.github.com>
2025-07-23 12:11:23 +0000

    Add fleet_management_app/lib/models/driver.dart 1753272680049

---
+++ fleet_management_app/lib/models/driver.dart
@@ -0,0 +1,13 @@
+class Driver {
+  final String id;
+  final String name;
+  final String licenseNumber;
+  final String phoneNumber;
+
+  Driver({
+    required this.id,
+    required this.name,
+    required this.licenseNumber,
+    required this.phoneNumber,
+  });
+}
